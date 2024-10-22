21e6fbe36969822125f549aed33d83fdbf8a2418
JairoAdrianVR <jairoavegaromero@gmail.com>
2024-10-21 19:23:47 -0600

    Update: CORS index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+    credentials: true,
+    origin: ['http://localhost:5173/']
+}
+
 
 app.use(express.json());
-
+app.use(cors(corsOptions));
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
